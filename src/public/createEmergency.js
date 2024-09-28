ba8e139e89dcde09d8d391c20c487ef202472f5c
Vishal Sharma <88317433+vishutrickcoder@users.noreply.github.com>
2024-09-28 07:29:51 -0700

    Update createEmergency.js

--- src/public/createEmergency.js
+++ src/public/createEmergency.js
@@ -60,7 +60,6 @@
 //     }
 // });
 
-const axios = require("axios")
 document.getElementById('apiForm').addEventListener('submit', async function (event) {
     event.preventDefault(); // Prevent the default form submission
 
